0a241c30fde85b65718d71d17dfcb358a104b1f9
louisz <tetra_shine@hotmail.com>
2019-03-09 14:22:54 +0800

    Fix major bug not working

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-//export { hay, HayTypes } './src/hay';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/hay.min.js');
-} else {
-  module.exports = require('./dist/hay.js');
-}
+export { hay, HayTypes } from './src/hay';
